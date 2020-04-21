e3a15df117ee73c244d366b695216cd001f431ab
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-21 19:24:15 +0200

    added initial nn template code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,1 +1,33 @@
-print("hello world!")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv('./DATA/fake_reg.csv')
+print(df.head())
+sns.pairplot(df)
+plt.show()
+
+# Features
+X = df[['feature1', 'feature2']].values
+
+# Label
+y = df['price'].values
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+scaler = MinMaxScaler()
+
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
