206fa3fef830874f655ff18b590bc572280e2397
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-22 15:08:51 +0200

    modified comments

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,31 +3,31 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
 
+# Import data set
 df = pd.read_csv('./DATA/fake_reg.csv')
-print(df.head())
-sns.pairplot(df)
-plt.show()
 
-# Features
+# Define features / input from data set
 X = df[['feature1', 'feature2']].values
 
-# Label
+# Define label / output from data set
 y = df['price'].values
 
-# Split
+# Split data set in training and test groups
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+# Scale input data to be between 0 and 1
+MinMaxScaler().fit(X_train)
+X_train = MinMaxScaler().transform(X_train)
+X_test = MinMaxScaler().transform(X_test)
 
-scaler = MinMaxScaler()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(4, activation='relu'),
+  tf.keras.layers.Dense(2, activation='relu'),
+  tf.keras.layers.Dense(1)
+])
 
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
 
 
 
