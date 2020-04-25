f33a6a16bb7a1bbd41aa6f4f0629819deec96962
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-25 17:35:43 +0200

    added code for testing and showcasing model performance + saving/loading model

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import load_model
 import tensorflow as tf
 
 # Import data set
@@ -39,4 +41,44 @@ model.fit(x=X_train, y=y_train, epochs=250, verbose=1)
 # Plot error vs. epochs
 loss_df = pd.DataFrame(model.history.history)
 loss_df.plot()
+plt.title("Training Loss per Epoch")
 plt.show()
+
+# Evaluation (training and test scores should be close to each other)
+# Loss (MSE) on training set
+training_score = model.evaluate(X_train, y_train, verbose=0)
+# Loss (MSE) on test set
+test_score = model.evaluate(X_test, y_test, verbose=0)
+print("Training MSE score: " + str(training_score))
+print("Test MSE score: " + str(test_score))
+
+# Make predictions on test data
+# Plot predictions and ground truth values of test set next to each other
+test_predictions = model.predict(X_test)
+test_predictions = pd.Series(test_predictions.reshape(300, ))
+test_groundtruths = pd.DataFrame(y_test, columns=["Test set groundtruths"])
+test_groundtruths = pd.concat([test_groundtruths, test_predictions], axis=1)
+test_groundtruths.columns = ["Test set groundtruths", "Test set predictions"]
+print(test_groundtruths)
+
+# Show error data
+# Should be (close to) a straight line
+sns.scatterplot(x="Test set groundtruths", y="Test set predictions", data=test_groundtruths)
+plt.show()
+
+print("MAE: " + str(
+    mean_absolute_error(test_groundtruths["Test set groundtruths"], test_groundtruths["Test set predictions"])))
+print("MSE: " + str(
+    mean_squared_error(test_groundtruths["Test set groundtruths"], test_groundtruths["Test set predictions"])))
+
+# Test model on future data
+new_data = [[998, 1000]]
+new_data = scaler.transform(new_data)
+print("Price of new data: " + str(model.predict(new_data)))
+
+# Save model
+model.save("my_price_calculator_model")
+
+# Load and use saved model
+loaded_model = load_model("my_price_calculator_model")
+print("Loaded model prediction: " + str(loaded_model.predict(new_data)))
