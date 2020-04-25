added code for compile, train, plot error + fixed scaling bug

NeuralNetwork.py
@@ -18,9 +18,10 @@ y = df['price'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Scale input data to be between 0 and 1
-MinMaxScaler().fit(X_train)
-X_train = MinMaxScaler().transform(X_train)
-X_test = MinMaxScaler().transform(X_test)
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 # Define layers with neurons and activation function
 model = tf.keras.models.Sequential()
@@ -28,3 +29,14 @@ model.add(tf.keras.layers.Dense(4, activation="relu"))
 model.add(tf.keras.layers.Dense(4, activation="relu"))
 model.add(tf.keras.layers.Dense(4, activation="relu"))
 model.add(tf.keras.layers.Dense(1))
+
+# Compile model with learning rate optimizer and loss function
+model.compile(optimizer="rmsprop", loss="mse")
+
+# Train the model
+model.fit(x=X_train, y=y_train, epochs=250, verbose=1)
+
+# Plot error vs. epochs
+loss_df = pd.DataFrame(model.history.history)
+loss_df.plot()
+plt.show()